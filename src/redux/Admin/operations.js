77e986b8fdfb83784e54b90d97cc1157ea071360
Andrii-Pershko <andriiinf@gmail.com>
2023-10-23 18:36:36 +0300

    add middleware check

--- src/redux/Admin/operations.js
+++ src/redux/Admin/operations.js
@@ -3,7 +3,35 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://sveeton-products.onrender.com';
 
+export const addProduct = createAsyncThunk(
+  'autorizationAdmin',
+  async (formData, thunkAPI) => {
+    console.log('formData', formData);
+    try {
+      const response = await axios.post(`/super_admin`, formData);
+
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
 export const autorizationAdmin = createAsyncThunk(
+  'autorizationAdmin',
+  async (formData, thunkAPI) => {
+    console.log('formData', formData);
+    try {
+      const response = await axios.post(`/super_admin`, formData);
+
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const updateProduct = createAsyncThunk(
   'autorizationAdmin',
   async (formData, thunkAPI) => {
     console.log('formData', formData);
